0b4068f95d4335fc4d5b440465fcbad10634a05a
Chris Dieckhaus <christopherdieckhaus@gmail.com>
2014-11-20 22:29:52 +0000

    Set up heap initializer

--- play.c
+++ play.c
@@ -2,26 +2,40 @@
 #include <stdlib.h>
 #include <stdio.h>
 
+//type: 0 = null, 1 = IND, 2 = INT
+
 struct node {
-	int data;
+	int address;
+	int val2;
+	int val1;
+	int type;
+	const char *typ;
 	struct node *next;
 	struct node *prev;
 };
+	
 
 struct node *head = NULL;
 	
 
 int main(int argc, char** argv){
 	
-	createList(10);
+	initHeap(10);	//parameter is total space in memory (# of links in LL)
 	
 	return 0;
 }
 
-void createList(int length){
+void initHeap(int length){
+	//add heap objects
+	int allocate = 4;
+	int types[4] = {1,1,2,1};
+	int val1s[4] = {2,5,10,3};
+	int val2s[4] = {0,0,0,0};
+	char *typs[] = {"IND","IND","INT","IND"};
+
 	if (head==NULL){
 		head = malloc(sizeof(struct node));
-		head->data = 0;
+		head->address = 0;
 		head->next = NULL;
 		head->prev = NULL;
 	}
@@ -29,9 +43,14 @@ void createList(int length){
 	curr = head;
 	
 	int i;
-	for (i=1; i<length; i++){
+	for (i=0; i<length; i++){
 		struct node * ptr = malloc(sizeof(struct node));
-		ptr->data = i;
+		ptr->address = 0;
+		ptr->type = 0;
+		ptr->val1 = 0;
+		ptr->val2 = 0;
+		ptr->typ = NULL;
+
 		ptr->next = NULL;
 		ptr->prev = curr;
 		curr->next = ptr;
@@ -40,8 +59,23 @@ void createList(int length){
 	curr->next = head;
 	head->prev = curr;
 	curr = head;
-	while (curr != NULL){
-		printf("%d ", curr->data);
+	
+	int j;
+	for (j=0; j<4; j++){
+
+		curr->address = j*3;
+		curr->type = types[j];
+		curr->val1 = val1s[j];
+		curr->val2 = 0;
+		const char *ty = typs[j];
+		curr->typ = ty;
+
+		curr = curr->next;
+	}
+	curr = head;
+
+	for (i=0; i<length; i++){
+		printf("%d | %s | %d | %d\n", curr->address, curr->typ, curr->val1, curr->val2);
 		curr = curr->next;
 	}
 }
